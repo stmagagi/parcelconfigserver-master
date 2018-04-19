f88225f363c9c0ef4a9adf7a5983fd4a05ca05db
Paul Buskovskij <paul.buskovskij@gmail.com>
2018-04-19 15:09:43 +0200

    Initial Size Creation

--- server.js
+++ server.js
@@ -12,15 +12,30 @@ app.route('/api/getGirthSize').get(function (req, res) {
     if (self) {
         let input = self["length"] + 2 * self["width"] + 2 * self["height"];
         size["findOne"]({from: {$lte: input}, to: {$gte: input}}, function (err, size) {
-            if (err) res.status(500).send(err);
+            if (err) res.status(503).send(err);
             else if (!size) res.json({size: "---"});
             else res.json({size: size.title});
         });
-    } else res.end();
+    } else res.status(428).send("Es wurde keine Größe übergeben.");
 });
 
 app.route("/*").get(function (req, res) {
     res.redirect('/');
+});
+
+function createSize(params) {
+    let newSize = new size(params);
+    newSize.save();
+}
+
+size.count({}, function (err, counter) {
+    if (counter < 1) {
+        createSize({from: 0, to: 35, title: "XS"});
+        createSize({from: 35, to: 50, title: "S"});
+        createSize({from: 50, to: 65, title: "M"});
+        createSize({from: 65, to: 80, title: "L"});
+        createSize({from: 80, to: 300, title: "XL"});
+    }
 });
 
 app.listen(port, function () {
@@ -30,4 +45,5 @@ app.listen(port, function () {
 
 process.on("SIGINT", function () {
     process.kill(process.pid, "SIGTERM");
+    mongoose.connection.close();
 });
