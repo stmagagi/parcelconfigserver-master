a1adbfa956a215afd544fd1a939b3ca49328a2fb
Paul Buskovskij <paul.buskovskij@gmail.com>
2018-04-30 12:46:14 +0200

    Server refactoring

--- models/size.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (mongoose) {
-    var Schema = mongoose.Schema;
-    var size = new Schema({
-        from: Number,
-        to: Number,
-        title: String
-    });
-    return mongoose.model('size', size);
-};
--- server.js
+++ server.js
@@ -1,49 +1,13 @@
 let express = require("express");
-let mongoose = require("mongoose");
-let promise = mongoose.connect('mongodb://localhost/pcs');
-let size = require('./models/size')(mongoose);
 let app = express();
-let port = 8080;
+let port = 8081;
 
-app.use(express.static('../ParcelConfigService/'));
+app.use(express.static('./'));
 
-app.route('/api/getGirthSize').get(function (req, res) {
-    let self = JSON.parse(req.query.size);
-    if (self) {
-        let input = self["length"] + 2 * self["width"] + 2 * self["height"];
-        size["findOne"]({from: {$lte: input}, to: {$gte: input}}, function (err, size) {
-            if (err) res.status(503).send(err);
-            else if (!size) res.json({size: "---"});
-            else res.json({size: size.title});
-        });
-    } else res.status(428).send("Es wurde keine Größe übergeben.");
-});
-
-app.route("/*").get(function (req, res) {
+app.route('/*').get(function (req, res) {
     res.redirect('/');
 });
 
-function createSize(params) {
-    let newSize = new size(params);
-    newSize.save();
-}
-
-size.count({}, function (err, counter) {
-    if (counter < 1) {
-        createSize({from: 0, to: 35, title: "XS"});
-        createSize({from: 35, to: 50, title: "S"});
-        createSize({from: 50, to: 65, title: "M"});
-        createSize({from: 65, to: 80, title: "L"});
-        createSize({from: 80, to: 300, title: "XL"});
-    }
-});
-
 app.listen(port, function () {
-    console.info(promise);
     console.info("http://localhost:" + port);
-});
-
-process.on("SIGINT", function () {
-    process.kill(process.pid, "SIGTERM");
-    mongoose.connection.close();
 });
